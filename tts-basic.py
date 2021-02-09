506f22357ccc7be973a9abf9a178fb84a8a38f44
Nelson Gou <nel4gou@gmail.com>
2021-02-08 16:26:08 -0800

    Cosmetic Changes Only

--- tts-basic.py
+++ tts-basic.py
@@ -1,9 +1,18 @@
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# File:             tts-basic.py
+# Description:      Generates an mp3 file using a specified voice from the Google Text-to-Speech API
+# Author:           Nelson Gou, Eric Zhang
+# Creation Date:    1/18/2021
+#
+# Copyright © 2020-2021 Nelson Gou, Eric Zhang. All rights reserved.
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
 import os
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="/Users/nelsongou/Desktop/tts-key.json"
 from google.cloud import texttospeech
 
-prm_text = "water" # text which the output mp3 will say
-prm_voice = "ru-RU-Wavenet-B" # look here: https://cloud.google.com/text-to-speech/docs/voices
+prm_text = "food" # text which the output mp3 will say
+prm_voice = "en-US-Wavenet-B" # look here: https://cloud.google.com/text-to-speech/docs/voices
 prm_speed = 1.0 # speed: 0.25 - 4.0, default = 1.0
 prm_pitch = 0.0 # pitch: -20.0 - 20.0, default = 0.0
 
@@ -11,27 +20,23 @@ prm_pitch = 0.0 # pitch: -20.0 - 20.0, default = 0.0
 client = texttospeech.TextToSpeechClient()
 
 # Set the text input to be synthesized
-synthesis_input = texttospeech.SynthesisInput(
-    text=prm_text)
+synthesis_input = texttospeech.SynthesisInput(text=prm_text)
 
 # Build the voice request
 voice = texttospeech.VoiceSelectionParams(
 name=prm_voice, 
-language_code=prm_voice[:prm_voice.find('-', prm_voice.find('-')+1)], 
-)
+language_code=prm_voice[:prm_voice.find('-', prm_voice.find('-')+1)])
 
 # Select the type of audio file you want returned
 audio_config = texttospeech.AudioConfig(
 audio_encoding=texttospeech.AudioEncoding.MP3,
 speaking_rate=prm_speed,
-pitch=prm_pitch 
-)
+pitch=prm_pitch)
 
 # Perform the text-to-speech request on the text input with the selected
 # voice parameters and audio file type
 response = client.synthesize_speech(
-input=synthesis_input, voice=voice, audio_config=audio_config
-)
+input=synthesis_input, voice=voice, audio_config=audio_config)
 
 # The response's audio_content is binary.
 outfile = prm_voice + '.mp3'
